85917b666a10e0e92fc0d8174f7ef9a468e3c439
Rootul Patel <rootulp@palantir.com>
2019-01-04 20:26:41 -0800

    Extract tokens

--- board.py
+++ board.py
@@ -1,17 +1,21 @@
 class Board:
 
+    PLAYER_TOKEN = 'X'
+    BOT_TOKEN = 'O'
+    EMPTY_TOKEN = '.'
+
     def __init__(self):
         self.board = self.initial_board()
 
     def player_move(self, row, col):
-        self.make_move('X', row, col)
+        self.make_move(self.PLAYER_TOKEN, row, col)
 
     def bot_make_move(self):
         row, col = self.next_open_position()
         self.bot_move(row, col)
 
     def bot_move(self, row, col):
-        self.make_move('O', row, col)
+        self.make_move(self.BOT_TOKEN, row, col)
 
     def make_move(self, token, row, col):
         self.board[row][col] = token
@@ -19,7 +23,7 @@ class Board:
     def next_open_position(self):
         for row_i, row in enumerate(self.board):
             for col_i, token in enumerate(row):
-                if token == '.':
+                if token == self.EMPTY_TOKEN:
                     return row_i, col_i
 
         raise 'No open positions'
