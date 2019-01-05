53f1b3de8b23403e2f6f735b6fb654d9df742b42
Rootul Patel <rootulp@palantir.com>
2019-01-05 14:56:56 -0800

    Replace end state methods with geeksforgeeks impl

--- board.py
+++ board.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Board:
 
     PLAYER_TOKEN = 'X'
@@ -33,36 +35,57 @@ class Board:
                 [self.EMPTY_TOKEN, self.EMPTY_TOKEN, self.EMPTY_TOKEN],
                 [self.EMPTY_TOKEN, self.EMPTY_TOKEN, self.EMPTY_TOKEN]]
 
-    def score(self):
-        if not self.is_game_over():
-            return 0
+    # Evaluates whether there is a winner or a tie
+    def evaluate_board(self):
+        winner = 0
 
-    def is_game_over(self):
-        return self.any_three_in_row() or self.any_three_in_col() or self.any_three_in_diag()
+        for player in (self.PLAYER_TOKEN, self.BOT_TOKEN):
+            if (self.row_win(player) or
+                self.col_win(player) or
+                self.diag_win(player)):
 
-    def any_three_in_row(self):
-        return self.is_three_in_row(0) or self.is_three_in_row(1) or self.is_three_in_row(2)
+                winner = player
 
-    def is_three_in_row(self, row_i):
-        not_empty = not self.is_empty_position(row_i, 0)
-        three_in_row = self.board[row_i][0] == self.board[row_i][1] == self.board[row_i][2]
-        return not_empty and three_in_row
+        if np.all(self.board != self.EMPTY_TOKEN) and winner == 0:
+            winner = -1
+        return winner
 
-    def any_three_in_col(self):
-        return self.is_three_in_col(0) or self.is_three_in_col(1) or self.is_three_in_col(2)
+    # Checks whether the player has three of their marks in a horizontal row
+    def row_win(self, player):
+        for x in range(len(self.board)):
+            win = True
 
-    def is_three_in_col(self, col_i):
-        not_empty = not self.is_empty_position(0, col_i)
-        three_in_col = self.board[0][col_i] == self.board[1][col_i] == self.board[2][col_i]
-        return not_empty and three_in_col
+            for y in range(len(self.board)):
+                if self.board[x][y] != player:
+                    win = False
+                    continue
 
-    def any_three_in_diag(self):
-        # There are only two diagonals in the board
-        not_empty_diag_1 = not self.is_empty_position(0, 0)
-        three_in_diag_1 = self.board[0][0] == self.board[1][1] == self.board[2][2]
-        not_empty_diag_2 = not self.is_empty_position(2, 0)
-        three_in_diag_2 = self.board[2][0] == self.board[1][1] == self.board[0][2]
-        return (not_empty_diag_1 and three_in_diag_1) or (not_empty_diag_2 and three_in_diag_2)
+            if win == True:
+                return(win)
+        return(win)
+
+    # Checks whether the player has three of their marks in a vertical row
+    def col_win(self, player):
+        for x in range(len(self.board)):
+            win = True
+
+            for y in range(len(self.board)):
+                if self.board[y][x] != player:
+                    win = False
+                    continue
+
+            if win == True:
+                return(win)
+        return(win)
+
+    # Checks whether the player has three of their marks in a diagonal row
+    def diag_win(self, player):
+        win = True
+
+        for x in range(len(self.board)):
+            if self.board[x][x] != player:
+                win = False
+        return(win)
 
     def is_empty_position(self, row_i, col_i):
         return self.board[row_i][col_i] == self.EMPTY_TOKEN
