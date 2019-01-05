25be48e45ce40bd30f4e8d9036def16d25a3c5f3
Rootul Patel <rootulp@palantir.com>
2019-01-04 20:38:39 -0800

    Duplicative code with bug b/c empty_token

--- board.py
+++ board.py
@@ -34,7 +34,25 @@ class Board:
                 [self.EMPTY_TOKEN, self.EMPTY_TOKEN, self.EMPTY_TOKEN]]
 
     def is_game_over(self):
-        pass
+        return self.any_three_in_row() or self.any_three_in_col() or self.any_three_in_diag()
+
+    def any_three_in_row(self):
+        return self.is_three_in_row(0) or self.is_three_in_row(1) or self.is_three_in_row(2)
+
+    def is_three_in_row(self, row_i):
+        return self.board[row_i][0] == self.board[row_i][1] == self.board[row_i][2]
+
+    def any_three_in_col(self):
+        return self.is_three_in_col(0) or self.is_three_in_col(1) or self.is_three_in_col(2)
+
+    def is_three_in_col(self, col_i):
+        return self.board[0][col_i] == self.board[1][col_i] == self.board[2][col_i]
+
+    def any_three_in_diag(self):
+        # There are only two diagonals in the board
+        diagonal_1 = self.board[0][0] == self.board[1][1] == self.board[2][2]
+        diagonal_2 = self.board[2][0] == self.board[1][1] == self.board[0][2]
+        return diagonal_1 or diagonal_2
 
     def __str__(self):
         output = ['Board:']
